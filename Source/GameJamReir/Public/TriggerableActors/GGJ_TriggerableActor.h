a306352170d097db74a2be5284aa3fdfe6dc9bae
Michelle Bohórquez <michii_juana@hotmail.com>
2024-01-26 19:54:57 -0500

    C++ Setup
    First setup for C++ and VS tools

---
+++ Source/GameJamReir/Public/TriggerableActors/GGJ_TriggerableActor.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerSphere.h"
+#include "GGJ_TriggerableActor.generated.h"
+
+/**
+ * Base class for any actor that can be triggered by the player
+ */
+UCLASS()
+class GAMEJAMREIR_API AGGJ_TriggerableActor : public ATriggerSphere
+{
+	GENERATED_BODY()
+
+protected:
+};
